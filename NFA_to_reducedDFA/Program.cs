858fdc3a3c9d31c1b977c0e721cc47b684e4f159
momo990305@gmail.com <momo990305@gmail.com>
2022-05-08 15:46:16 +0900

    First Upload

---
+++ NFA_to_reducedDFA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NFA_to_reducedDFA
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
