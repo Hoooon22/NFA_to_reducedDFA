e08c7503ab5736d0adff619fed7aeece917c4e10
momo990305@gmail.com <momo990305@gmail.com>
2022-05-11 13:49:58 +0900

    input number of states & transition

--- NFA_to_reducedDFA/Program.cs
+++ NFA_to_reducedDFA/Program.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NFA_to_reducedDFA
 {
     class NFA
     {
+        private Dictionary<string, List<string>> nfa = new Dictionary<string, List<string>>();
+        private int s; // number of states
+        private int t; // number of transitions
 
+        public void Enter_nfa()
+        {
+            // Input states and transitions
+            Console.Write("Enter the number of states: ");
+            s = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of transitions: ");
+            t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < s; i++)
+            {
+                
+            }
+        }
     }
 
     class MainClass
