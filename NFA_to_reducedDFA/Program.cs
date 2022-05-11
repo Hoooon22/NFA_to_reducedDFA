fc40ef505c4eff33a369fd1ba714cf7ec8ac95ce
momo990305@gmail.com <momo990305@gmail.com>
2022-05-11 15:03:22 +0900

    Create NFA complete

--- NFA_to_reducedDFA/Program.cs
+++ NFA_to_reducedDFA/Program.cs
@@ -5,7 +5,7 @@ namespace NFA_to_reducedDFA
 {
     class NFA
     {
-        private Dictionary<string, List<string>> nfa = new Dictionary<string, List<string>>();
+        private Dictionary<string, Dictionary<string, string[]>> nfa_dic = new Dictionary<string, Dictionary<string, string[]>>();
         private int s; // number of states
         private int t; // number of transitions
 
@@ -17,9 +17,36 @@ namespace NFA_to_reducedDFA
             Console.Write("Enter the number of transitions: ");
             t = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < s; i++)
+            // Enter name of state and transition
+            for (int i = 0; i < s; i++) // state
             {
-                
+                Console.Write("\n" + i + ". state name: ");
+                string state = Console.ReadLine();
+
+                Dictionary<string, string[]> t_dic = new Dictionary<string, string[]>(); // 임시 저장
+                for (int j = 0; j < t; j++) // transition
+                {
+                    Console.Write("\n" + i + ". transition name: ");
+                    string transition = Console.ReadLine();
+                    Console.Write("next state from state " + state + " through " + transition + ": ");
+                    string next_state = Console.ReadLine();
+                    t_dic.Add(transition, next_state.Split(' '));
+                }
+
+                nfa_dic.Add(state, t_dic); // add nfa
+            }
+        }
+        public void Prinf_nfa()
+        {
+            foreach (var pair1 in nfa_dic)
+            {
+                Console.Write("Key: {0}, ", pair1.Key);
+
+                foreach (var pair2 in pair1.Value)
+                {
+                    Console.Write("Value: {0}: [{1}]", pair2.Key, String.Join(", ", pair2.Value)); ;
+                }
+                Console.WriteLine();
             }
         }
     }
@@ -28,7 +55,9 @@ namespace NFA_to_reducedDFA
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NFA nfa = new NFA();
+            nfa.Enter_nfa();
+            nfa.Prinf_nfa();
         }
     }
 }
